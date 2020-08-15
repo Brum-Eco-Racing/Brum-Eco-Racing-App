f10ce779212d26d35eb0c130dba9de7b9e309246
Liam Mack <liam.mackenzie@icloud.com>
2020-08-15 21:20:20 +0100

    Initial Commit

---
+++ Brum Eco Racing/Brum_Eco_RacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brum_Eco_RacingApp.swift
+//  Brum Eco Racing
+//
+//  Created by Liam Mackenzie on 8/15/20.
+//
+
+import SwiftUI
+
+@main
+struct Brum_Eco_RacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brum Eco Racing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Brum Eco Racing
+//
+//  Created by Liam Mackenzie on 8/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
